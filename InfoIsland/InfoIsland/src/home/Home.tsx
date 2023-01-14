1b3c389103cd835d314080ccb1359a4511c9746b
Kaikei-e <e_kaikei@protonmail.com>
2023-01-14 22:48:12 +0900

    Update: styling the home page of left side

--- InfoIsland/InfoIsland/src/home/Home.tsx
+++ InfoIsland/InfoIsland/src/home/Home.tsx
@@ -1,4 +1,4 @@
-import {Flex, Text} from "@chakra-ui/react";
+import {Box, Center, Container, Flex, Text} from "@chakra-ui/react";
 
 const Home = () => {
     return (
@@ -7,11 +7,72 @@ const Home = () => {
               h={'100vh'}
               minH={'100%'}
               minW={'100%'}
+              flexDir={'row'}
               p={'2%'}>
-            <Text fontFamily="Jost"
-                  fontWeight="regular"
-                  fontSize={{base: "xl", md: "2xl", lg: "5xl"}}
-                  color="#000">Home</Text>
+            <Flex flexDirection={"column"} w={"50%"} h={"100%"}>
+                <Text fontFamily="Jost"
+                      fontWeight="regular"
+                      fontSize={{base: "xl", md: "2xl", lg: "5xl"}}
+                      color="#000"
+                      m={"2%"}>
+                    Home
+                </Text>
+                <Flex w={"100%"} h={"40%"}
+                      m={"2%"} justifyContent={"center"}
+                >
+                    <Flex w={"90%"} h={"100%"} bgColor={"#CDFFD7"} rounded={"xl"}
+                          justifyContent={"center"} alignItems={"center"}>
+                        <Flex justifyContent={"start"} w={"100%"} h={"100%"} p={"1%"}>
+                            <Box>
+                                <Text
+                                    fontWeight="regular"
+                                    fontSize={{base: "xl", md: "2xl", lg: "2xl"}}
+                                    color="#000">
+                                    Curiosity
+                                </Text>
+                            </Box>
+                        </Flex>
+                    </Flex>
+                </Flex>
+                <Flex w={"100%"} h={"40%"}
+                      m={"2%"} justifyContent={"center"}
+                >
+                    <Flex justifyContent={"start"} w={"90%"} h={"100%"} flexDirection={"row"}>
+                        <Flex bgColor={"#FFE4C3"}
+                              w={"48%"} h={"100%"}
+                              p={"1%"} mr={"2%"} rounded={"xl"}
+                        >
+                            <Flex justifyContent={"start"} alignItems={"start"}>
+                                <Text
+                                    fontWeight="regular"
+                                    fontSize={{base: "xl", md: "2xl", lg: "2xl"}}
+                                    color="#000">
+                                    FeedFlare: Feed timeline
+                                </Text>
+                            </Flex>
+                        </Flex>
+                        <Flex flexDirection={"column"}
+                              bgColor={"#FFE4C3"}
+                              w={"48%"} h={"100%"}
+                              p={"1%"} ml={"2%"} rounded={"xl"}
+                              justifyContent={"center"} alignItems={"center"}>
+                            <Box bgColor={"#D5D5D5"}
+                                 w={"100%"} h={"48%"} mb={"2%"}>
+                                <Text>
+                                    SlateFlex: Notes
+                                </Text>
+                            </Box>
+                            <Box bgColor={"#D5D5D5"}
+                                 w={"100%"} h={"48%"} mt={"2%"}>
+                                <Text>
+                                    Account Settings
+                                </Text>
+                            </Box>
+                        </Flex>
+                    </Flex>
+                </Flex>
+            </Flex>
+
         </Flex>
 
     )
