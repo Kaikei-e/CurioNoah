e4fb1421c431988558ec505aecd4ba89c9b67680
Kaikei-e <e_kaikei@protonmail.com>
2023-01-16 01:48:26 +0900

    Update: Just CORS issue. Fixed the issue

--- FeedFlare/collect-service/server/server.go
+++ FeedFlare/collect-service/server/server.go
@@ -4,6 +4,7 @@ import (
 	"feedflare/collector/fetchFeeds"
 	"feedflare/collector/testdata"
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 	"github.com/mmcdole/gofeed"
 )
 
@@ -15,6 +16,18 @@ func Server() {
 	if err != nil {
 		e.Logger.Errorf("error: %v. maybe serer is down", err)
 	}
+
+	corsConfig := middleware.CORSConfig{
+		AllowOrigins: []string{"http://localhost:5173"},
+		AllowHeaders: []string{
+			echo.HeaderOrigin,
+			echo.HeaderContentType,
+			echo.HeaderAccessControlAllowHeaders,
+			echo.HeaderAccept},
+		AllowMethods: []string{echo.GET, echo.PUT, echo.POST, echo.DELETE},
+	}
+
+	e.Use(middleware.CORSWithConfig(corsConfig))
 
 	apiV1 := e.Group("/api/v1")
 	apiV1.Use()
@@ -31,6 +44,10 @@ func Server() {
 				feedFormatted := *feed
 				feedsFormatted = append(feedsFormatted, feedFormatted)
 			}
+
+			c.Response().Header().Set(echo.HeaderContentType, echo.MIMEApplicationJSONCharsetUTF8)
+			c.Response().Header().Set("Access-Control-Allow-Origin", c.Request().Header.Get("Origin"))
+			c.Response().Header().Set("Access-Control-Allow-Headers", "Content-Type, Origin, Accept")
 
 			return c.JSON(200, feedsFormatted)
 		})
