94287d4ce9d94dc6fb5dbe66ebf23f4d01705528
Kaikei-e <e_kaikei@protonmail.com>
2023-01-16 22:18:15 +0900

    Update: generated the struct of following list by ent.

--- FeedFlare/collect-service/ent/schema/followlist.go
+++ FeedFlare/collect-service/ent/schema/followlist.go
@@ -1,6 +1,11 @@
 package schema
 
-import "entgo.io/ent"
+import (
+	"entgo.io/ent"
+	"entgo.io/ent/schema/field"
+	"github.com/google/uuid"
+	"time"
+)
 
 // FollowList holds the schema definition for the FollowList entity.
 type FollowList struct {
@@ -9,7 +14,24 @@ type FollowList struct {
 
 // Fields of the FollowList.
 func (FollowList) Fields() []ent.Field {
-	return nil
+	return []ent.Field{
+		field.UUID("uuid", uuid.UUID{}).Default(uuid.New),
+		field.Int8("xml_version").Default(1),
+		field.Int8("rss_version").Default(2),
+		field.String("url"),
+		field.String("title"),
+		field.String("description"),
+		field.String("link"),
+		field.String("links"),
+		field.String("language"),
+		field.Time("dt_created").Default(time.Now()),
+		field.Time("dt_updated").Default(time.Now()),
+		field.Int("feed_category").Default(0),
+		field.Bool("is_active").Default(true),
+		field.Bool("is_favorite").Default(false),
+		field.Bool("is_read").Default(false),
+		field.Bool("is_updated").Default(false),
+	}
 }
 
 // Edges of the FollowList.
