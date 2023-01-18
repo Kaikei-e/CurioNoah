58d610f99ace789b456a1e0e7c925de01d599cbc
Kaikei-e <e_kaikei@protonmail.com>
2023-01-18 23:35:41 +0900

    Update: Implementing the feed register

--- FeedFlare/FeedFlare/collector/testdata/feedList.go
+++ FeedFlare/FeedFlare/collector/testdata/feedList.go
@@ -5,4 +5,13 @@ package testdata
 var FeedList = []string{
 	"https://rss.nytimes.com/services/xml/rss/nyt/World.xml",
 	"https://rss.nytimes.com/services/xml/rss/nyt/US.xml",
+	"https://zenn.dev/topics/recoil/feed",
+	"https://zenn.dev/topics/react/feed",
+	"https://zenn.dev/topics/nextjs/feed",
+	"https://zenn.dev/topics/go/feed",
+	"https://zenn.dev/topics/typescript/feed",
+	"https://zenn.dev/topics/rust/feed",
+	"https://zenn.dev/topics/nodejs/feed",
+	"https://zenn.dev/topics/docker/feed",
+	"https://zenn.dev/topics/linux/feed",
 }
