7c706f3c170787c90c72fc53152b380541deb7a0
Kaikei-e <e_kaikei@protonmail.com>
2023-01-18 23:37:58 +0900

    Update: added the origins of localhost

--- FeedFlare/FeedFlare/server/server.go
+++ FeedFlare/FeedFlare/server/server.go
@@ -20,7 +20,8 @@ func Server() {
 	}
 
 	corsConfig := middleware.CORSConfig{
-		AllowOrigins: []string{"http://localhost:5173", "http://127.0.0.1:5173"},
+		AllowOrigins: []string{"http://localhost:5173", "http://127.0.0.1:5173",
+			"http://localhost:4173", "http://127.0.0.1:4173"},
 		AllowHeaders: []string{
 			echo.HeaderOrigin,
 			echo.HeaderContentType,
