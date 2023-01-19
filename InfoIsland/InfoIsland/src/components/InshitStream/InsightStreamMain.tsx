8860591d9e4b6de6fba0fbd79c854ceb861d8a66
Kaikei-e <e_kaikei@protonmail.com>
2023-01-19 22:59:39 +0900

    Update: Styling the InsightStream main component

--- InfoIsland/InfoIsland/src/components/InshitStream/InsightStreamMain.tsx
+++ InfoIsland/InfoIsland/src/components/InshitStream/InsightStreamMain.tsx
@@ -1,4 +1,10 @@
-import {Flex, Text} from "@chakra-ui/react";
+import {
+    Flex, Text, Table,
+    Thead, Tbody, Tfoot,
+    Tr, Th, Td,
+    TableCaption,
+    TableContainer,
+} from "@chakra-ui/react";
 import React from "react";
 
 const InsightStreamMain = () => {
@@ -7,26 +13,82 @@ const InsightStreamMain = () => {
               minH={"100%"} minW={"100%"}
               overflowX={"hidden"}
               flexDirection={"row"}
+              fontFamily="Jost"
         >
             <Flex flexDirection={"column"} w={"20%"} h={"100%"} bgColor={"#88A6B1"}>
                 <Flex h={"6%"}
                       justifyContent={"center"} alignItems={"center"}
-                      rounded={"xl"} m={"5%"} p={"1%"}
+                      rounded={"xl"} p={"4%"}
                       textAlign={"center"}
                 >
-                    <Text fontFamily="Jost"
-                          fontWeight="regular"
-                          fontSize={{base: "lg", md: "xl", lg: "4xl"}}
-                          color="#fff"
-                          textAlign={"start"}>
+                    <Text
+                        style={{
+                            borderBottom: "2px solid #000"
+                        }}
+                        fontWeight="regular"
+                        fontSize={{base: "lg", md: "xl", lg: "4xl"}}
+                        color="#fff"
+                        textAlign={"start"}>
                         InsightStream
                     </Text>
                 </Flex>
                 <Flex flexDirection={"column"}
                       w={"100%"} h={"94%"}
-                      rounded={"xl"} p={"2%"}
+                      alignItems={"center"}
+                      p={"4%"}
                 >
+                    <Flex flexDirection={"column"}
+                          w={"80%"} h={"60%"}
+                          bgColor={"#EAF2F8"}
+                          rounded={"xl"}
+                          border={"1px solid #000"}
+                    >
+                        <Text fontWeight="regular"
+                              fontSize={{base: "xl", md: "xl", lg: "2xl"}}
+                              color="#000" m={"2%"}
+                              textAlign={"center"}>
+                            Status
+                        </Text>
+                        <TableContainer>
+                            <Table variant='simple'>
+                                <TableCaption>Imperial to metric conversion factors</TableCaption>
+                                <Thead>
+                                    <Tr>
+                                        <Th>To convert</Th>
+                                        <Th>into</Th>
+                                        <Th isNumeric>multiply by</Th>
+                                    </Tr>
+                                </Thead>
+                                <Tbody>
+                                    <Tr>
+                                        <Td>inches</Td>
+                                        <Td>millimetres (mm)</Td>
+                                        <Td isNumeric>25.4</Td>
+                                    </Tr>
+                                    <Tr>
+                                        <Td>feet</Td>
+                                        <Td>centimetres (cm)</Td>
+                                        <Td isNumeric>30.48</Td>
+                                    </Tr>
+                                    <Tr>
+                                        <Td>yards</Td>
+                                        <Td>metres (m)</Td>
+                                        <Td isNumeric>0.91444</Td>
+                                    </Tr>
+                                </Tbody>
+                                <Tfoot>
+                                    <Tr>
+                                        <Th>To convert</Th>
+                                        <Th>into</Th>
+                                        <Th isNumeric>multiply by</Th>
+                                    </Tr>
+                                </Tfoot>
+                            </Table>
+                        </TableContainer>
+                    </Flex>
+
                 </Flex>
+
             </Flex>
             <Flex flexDirection={"column"}
                   w={"80%"} h={"100%"}
@@ -38,6 +100,7 @@ const InsightStreamMain = () => {
                 >
                 </Flex>
             </Flex>
+
         </Flex>
     );
 
