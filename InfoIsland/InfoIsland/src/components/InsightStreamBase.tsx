af45887c2052a10d69e7af30d37239d75e96b214
Kaikei-e <e_kaikei@protonmail.com>
2023-01-21 07:12:04 +0900

    Update: Add error handling. will improve that

--- InfoIsland/InfoIsland/src/components/InsightStreamBase.tsx
+++ InfoIsland/InfoIsland/src/components/InsightStreamBase.tsx
@@ -1,7 +1,7 @@
-import React, { useEffect } from 'react';
-import { Flex, Spinner } from "@chakra-ui/react";
+import React, {useEffect} from 'react';
+import {Flex, Spinner} from "@chakra-ui/react";
 import Timeline from "./timeline/Timeline";
-import { Feed } from "../lib/models/feedModel";
+import {Feed} from "../lib/models/feedModel";
 
 
 const InsightStreamBase = () => {
@@ -28,6 +28,12 @@ const InsightStreamBase = () => {
                 },
             );
             const feeds = await response.json();
+
+            if (feeds == null) {
+                setError(new Error("No feeds found"));
+
+            }
+
             setData(feeds);
             setIsLoading(false);
         };
@@ -41,17 +47,17 @@ const InsightStreamBase = () => {
     let displayData;
 
     if (!isLoading) {
-        displayData = <Timeline data={data} />
+        displayData = <Timeline data={data}/>
     } else if (error) {
         displayData = <div>Something went wrong ...</div>
     } else {
-        displayData = <FetchingFeeds />
+        displayData = <FetchingFeeds/>
     }
 
     return (
         <Flex flexDirection={"column"} w={"100%"} h={"100%"}
-            bgColor={"#EAF2F8"} rounded={"xl"}
-            overflow={"scroll"} overflowX={"hidden"}>
+              bgColor={"#EAF2F8"} rounded={"xl"}
+              overflow={"scroll"} overflowX={"hidden"}>
             {displayData}
         </Flex>
     );
@@ -61,7 +67,7 @@ const InsightStreamBase = () => {
 function FetchingFeeds() {
     return (
         <Flex flexDirection={"column"}
-            w={"100%"} h={"100%"}>
+              w={"100%"} h={"100%"}>
             <Spinner
                 thickness='4px'
                 speed='0.65s'
