ab916e5eb75684b2aee345fbd0ad2fa5b6a211d3
Kaikei-e <e_kaieki@protonmail.com>
2023-02-18 17:06:44 +0900

    Update: add file to accord the DIP

---
+++ InsightStream/InsightStream/dependencyInversion/feedCollectionDI.go
@@ -0,0 +1,24 @@
+package dependencyInversion
+
+type (
+	FeedCollection interface {
+		FetchAll() error
+		FetchSingle() error
+	}
+
+	FeedCollectionImpl struct {
+	}
+)
+
+func NewFeedCollection() FeedCollection {
+	return &FeedCollectionImpl{}
+}
+
+func (f *FeedCollectionImpl) FetchAll() error {
+	return nil
+
+}
+
+func (f *FeedCollectionImpl) FetchSingle() error {
+	return nil
+}
