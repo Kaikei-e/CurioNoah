dfa2c038cf82430dd20326f9e1ccc0bc0ff7476a
Kaikei-e <e_kaieki@protonmail.com>
2023-02-18 17:08:55 +0900

    Update: add func and impl to accord the DIP

--- InsightStream/InsightStream/adaptor/feedCollection/collectAll.go
+++ InsightStream/InsightStream/adaptor/feedCollection/collectAll.go
@@ -3,6 +3,7 @@ package feedCollection
 import (
 	"errors"
 	"github.com/labstack/echo/v4"
+	"insightstream/dependencyInversion"
 )
 
 func CollectAll(c echo.Context) error {
@@ -12,7 +13,10 @@ func CollectAll(c echo.Context) error {
 		return errors.New("context is nil")
 	}
 
-	err := c.JSON(200, "success")
+	f := dependencyInversion.NewFeedCollection()
+	err := f.FetchAll()
+
+	err = c.JSON(200, "success")
 	if err != nil {
 		return err
 	}
