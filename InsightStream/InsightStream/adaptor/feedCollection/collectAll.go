463d44b4c758dcbd92179030ab619c21846b2be5
Kaikei-e <e_kaieki@protonmail.com>
2023-02-18 16:39:55 +0900

    Update: wrote a failure test case

--- InsightStream/InsightStream/adaptor/feedCollection/collectAll.go
+++ InsightStream/InsightStream/adaptor/feedCollection/collectAll.go
@@ -1,9 +1,16 @@
 package feedCollection
 
-import "github.com/labstack/echo/v4"
+import (
+	"errors"
+	"github.com/labstack/echo/v4"
+)
 
 func CollectAll(c echo.Context) error {
 	// TODO
+
+	if c == nil {
+		return errors.New("context is nil")
+	}
 
 	err := c.JSON(200, "success")
 	if err != nil {
--- InsightStream/InsightStream/adaptor/feedCollection/collectAll_test.go
+++ InsightStream/InsightStream/adaptor/feedCollection/collectAll_test.go
@@ -1,6 +1,7 @@
 package feedCollection
 
 import (
+	"errors"
 	"github.com/labstack/echo/v4"
 	"net/http"
 	"net/http/httptest"
@@ -8,7 +9,7 @@ import (
 )
 
 func TestCollectAll(t *testing.T) {
-	// TODO
+	// TODO implement various test cases
 	e := echo.New()
 	req := httptest.NewRequest(http.MethodGet, "/api/v2/fetch-feed/stored-all", nil)
 	req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
@@ -24,11 +25,22 @@ func TestCollectAll(t *testing.T) {
 			input:   c,
 			wantErr: false,
 		},
+		"failure": {
+			input:   nil,
+			wantErr: true,
+		},
 	}
 
 	for name, c := range cases {
 		t.Run(name, func(t *testing.T) {
 			err := CollectAll(c.input)
+
+			if errors.Is(err, errors.New("context is nil")) {
+				t.Errorf("CollectAll() got = %v, want %v", err, errors.New("context is nil"))
+				return
+
+			}
+
 			if (err != nil) != c.wantErr {
 				t.Errorf("CollectAll() error = %v, wantErr %v", err, c.wantErr)
 				return
