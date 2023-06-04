d033f9ecc092bec01fc34cb3aea49e1411a3061e
Kaikei-e <e_kaikei@protonmail.com>
2023-06-04 19:36:43 +0900

    CuiroNoah: considering the multi user func and data structure

--- FeedHarmony/feed_harmony/src/api_handler/v1/store_feeds.rs
+++ FeedHarmony/feed_harmony/src/api_handler/v1/store_feeds.rs
@@ -3,13 +3,21 @@ use axum::{http::StatusCode, response::IntoResponse, Json};
 use serde::{Deserialize, Serialize};
 use uuid::Uuid;
 
-pub async fn store_feeds(Json(payload): Json<store_all>) -> impl IntoResponse {
+pub async fn store_feeds(Json(payload): Json<all_feeds>) -> impl IntoResponse {
+    // Payload is a just boolean. This is bad.
+    
+
     todo!()
 }
 
 #[derive(Clone, Debug, PartialEq, Default, Deserialize)]
 #[serde(rename_all = "camelCase")]
-pub struct store_all {
+pub struct all_feeds {
+    store: bool,
+}
+
+#[derive(Clone, Debug, PartialEq, Default, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct follow_list_by_id {
     user_id: Uuid,
-    store_all: bool,
 }
