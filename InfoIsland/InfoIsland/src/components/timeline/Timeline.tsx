48e0bb718958abc4588b1546520b1952c12aec26
Kaikei-e <e_kaikei@protonmail.com>
2023-06-13 01:05:20 +0900

    InsightStream: fix crashing when rss feed's description is empty

--- InfoIsland/InfoIsland/src/components/timeline/Timeline.tsx
+++ InfoIsland/InfoIsland/src/components/timeline/Timeline.tsx
@@ -1,14 +1,14 @@
-import {Container, Flex, Link, Text} from "@chakra-ui/react";
-import {ExternalLinkIcon} from '@chakra-ui/icons'
+import { Container, Flex, Link, Text } from "@chakra-ui/react";
+import { ExternalLinkIcon } from '@chakra-ui/icons'
 import React from "react";
-import {Feed, Item} from "../../lib/models/feedModel";
+import { Feed, Item } from "../../lib/models/feedModel";
 
 function Timeline(props: { data: Feed[]; }) {
 
     return (
 
         <Flex flexDirection={"column"} h={"100%"} w={"100%"}
-              fontFamily="Jost">
+            fontFamily="Jost">
             <Flex p={"2%"}>
                 <Text fontSize={{
                     base: "2xl",
@@ -17,7 +17,7 @@ function Timeline(props: { data: Feed[]; }) {
                 }}>
                     You can find the latest feeds that you have collected here.
                     Read more at <Text as='u'><Link
-                    href={"/insight-stream"}>InsightStream</Link></Text> page.
+                        href={"/insight-stream"}>InsightStream</Link></Text> page.
                 </Text>
             </Flex>
             {props.data.map((feed: Feed, index: number) => {
@@ -25,19 +25,18 @@ function Timeline(props: { data: Feed[]; }) {
                     <Flex flexDirection={"row"} key={index} m={"1%"}>
                         <Flex flexDirection={"row"} w={"100%"} h={"100%"}>
                             <Container maxW={"100%"} h={"100%"}
-                                       color={"#000"} p={"1%"}>
+                                color={"#000"} p={"1%"}>
                                 <Flex h={"20%"} w={"100%"}
-                                      flexDirection={"row"}>
+                                    flexDirection={"row"}>
                                     <Link href={feed.link} w={"50%"} h={"100%"}
-                                          overflowWrap={"break-word"} isExternal>
+                                        overflowWrap={"break-word"} isExternal>
                                         <Text fontSize={{
                                             base: "xl",
                                             md: "xl",
                                             lg: "xl"
                                         }}>
-                                            {/*&#x1F4DC; {feed.title}*/}
                                             &#x1F58B; {feed.title}
-                                            <ExternalLinkIcon mx={"2px"}/>
+                                            <ExternalLinkIcon mx={"2px"} />
                                         </Text>
                                     </Link>
                                     <Text w={"30%"} h={"100%"}>
@@ -45,20 +44,22 @@ function Timeline(props: { data: Feed[]; }) {
                                     </Text>
                                 </Flex>
                                 <Flex flexDirection={"column"} p={"1%"}
-                                      border={"1px"} borderRadius={"md"}
-                                      bgColor={"#fffbfb"} mb={"1%"}>
+                                    border={"1px"} borderRadius={"md"}
+                                    bgColor={"#fffbfb"} mb={"1%"}>
                                     {feed.items.map((item: Item, index: number) => {
                                         if (index > 3) {
                                             return;
                                         }
                                         return (
                                             <Flex flexDirection={"column"} key={index}
-                                                  mb={"1%"}>
+                                                mb={"1%"}>
                                                 <Link href={item.link} isExternal>
                                                     <Text>
                                                         &#x1F587;: {item.title}
-                                                        <ExternalLinkIcon mx={"2px"}/>
-                                                        / {item.description!.slice(0, 40)} ...
+                                                        <ExternalLinkIcon mx={"2px"} />
+                                                        / {
+                                                            item.description ? item.description.slice(0, 40) : "No Description"
+                                                        } ...
                                                     </Text>
                                                 </Link>
                                             </Flex>
