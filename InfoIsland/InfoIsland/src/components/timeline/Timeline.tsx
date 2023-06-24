b32e8f36c49c9c29526b1ea1941a28000db17700
Kaikei-e <e_kaikei@protonmail.com>
2023-06-25 02:19:18 +0900

    InfoIsland: adding the feature of infinite scroll

--- InfoIsland/InfoIsland/src/components/timeline/Timeline.tsx
+++ InfoIsland/InfoIsland/src/components/timeline/Timeline.tsx
@@ -2,11 +2,17 @@ import { Container, Flex, Link, Text } from "@chakra-ui/react";
 import { ExternalLinkIcon } from '@chakra-ui/icons'
 import React from "react";
 import { Feed, Item } from "../../lib/models/feedModel";
+import EachFeed from "./eachFeed";
+import {InfiniteScroll}  from "react-infinite-scroller";
 
 function Timeline(props: { data: Feed[]; }) {
+    const [list, setList] = React.useState<Feed[]>([]);
+
+    const loadMoreFeeds = (feeds: Feed[]) => {
+        setList([...list, feeds])
+    }
 
     return (
-
         <Flex flexDirection={"column"} h={"100%"} w={"100%"}
             fontFamily="Jost">
             <Flex p={"2%"}>
@@ -22,65 +28,11 @@ function Timeline(props: { data: Feed[]; }) {
             </Flex>
             {props.data.map((feed: Feed, index: number) => {
                 return (
-                    <Flex flexDirection={"row"} key={index} m={"1%"}>
-                        <Flex flexDirection={"row"} w={"100%"} h={"100%"}>
-                            <Container maxW={"100%"} h={"100%"}
-                                color={"#000"} p={"1%"}>
-                                <Flex h={"20%"} w={"100%"}
-                                    flexDirection={"row"}>
-                                    <Link href={feed.link} w={"50%"} h={"100%"}
-                                        overflowWrap={"break-word"} isExternal>
-                                        <Text fontSize={{
-                                            base: "xl",
-                                            md: "xl",
-                                            lg: "xl"
-                                        }}>
-                                            &#x1F58B; {feed.title}
-                                            <ExternalLinkIcon mx={"2px"} />
-                                        </Text>
-                                    </Link>
-                                    <Text w={"30%"} h={"100%"}>
-                                        {feed.published}
-                                    </Text>
-                                </Flex>
-                                <Flex flexDirection={"column"} p={"1%"}
-                                    border={"1px"} borderRadius={"md"}
-                                    bgColor={"#fffbfb"} mb={"1%"}>
-                                    {feed.items.map((item: Item, index: number) => {
-                                        if (index > 3) {
-                                            return;
-                                        }
-                                        return (
-                                            <Flex flexDirection={"column"} key={index}
-                                                mb={"1%"}>
-                                                <Link href={item.link} isExternal>
-                                                    <Text>
-                                                        &#x1F587;: {item.title}
-                                                        <ExternalLinkIcon mx={"2px"} />
-                                                        / {
-                                                            item.description ? item.description.slice(0, 40) : "No Description"
-                                                        } ...
-                                                    </Text>
-                                                </Link>
-                                            </Flex>
-                                        )
-                                    })}
-                                </Flex>
-                            </Container>
-                        </Flex>
-                    </Flex>
+                    <EachFeed feed={feed} index={index} />
                 );
             })}
         </Flex>
     );
 }
 
-// const ImageRenderer = (props: {
-//     url: string
-// }) => {
-//     return (
-//         // <Image src={base64Img} boxSize='30%' rounded={"xl"}/>
-//         <Image src={props.url} boxSize='40%' rounded={"xl"}/>
-//     )
-// }
 export default Timeline;
---
+++ InfoIsland/InfoIsland/src/components/timeline/eachFeed.tsx
@@ -0,0 +1,63 @@
+import {Container, Flex, Link, Text} from "@chakra-ui/react";
+import {ExternalLinkIcon} from "@chakra-ui/icons";
+import {Feed, Item} from "../../lib/models/feedModel";
+import React from "react";
+
+type EachFeedProps = {
+    feed: Feed;
+    index: number;
+}
+
+const EachFeed = (props: EachFeedProps ) => {
+    return (
+        <Flex flexDirection={"row"} key={props.index} m={"1%"}>
+            <Flex flexDirection={"row"} w={"100%"} h={"100%"}>
+                <Container maxW={"100%"} h={"100%"}
+                           color={"#000"} p={"1%"}>
+                    <Flex h={"20%"} w={"100%"}
+                          flexDirection={"row"}>
+                        <Link href={props.feed.link} w={"50%"} h={"100%"}
+                              overflowWrap={"break-word"} isExternal>
+                            <Text fontSize={{
+                                base: "xl",
+                                md: "xl",
+                                lg: "xl"
+                            }}>
+                                &#x1F58B; {props.feed.title}
+                                <ExternalLinkIcon mx={"2px"} />
+                            </Text>
+                        </Link>
+                        <Text w={"30%"} h={"100%"}>
+                            {props.feed.published}
+                        </Text>
+                    </Flex>
+                    <Flex flexDirection={"column"} p={"1%"}
+                          border={"1px"} borderRadius={"md"}
+                          bgColor={"#fffbfb"} mb={"1%"}>
+                        {props.feed.items.map((item: Item, index: number) => {
+                            if (index > 3) {
+                                return;
+                            }
+                            return (
+                                <Flex flexDirection={"column"} key={index}
+                                      mb={"1%"}>
+                                    <Link href={item.link} isExternal>
+                                        <Text>
+                                            &#x1F587;: {item.title}
+                                            <ExternalLinkIcon mx={"2px"} />
+                                            / {
+                                            item.description ? item.description.slice(0, 40) : "No Description"
+                                        } ...
+                                        </Text>
+                                    </Link>
+                                </Flex>
+                            )
+                        })}
+                    </Flex>
+                </Container>
+            </Flex>
+        </Flex>
+    )
+}
+
+export default EachFeed;
