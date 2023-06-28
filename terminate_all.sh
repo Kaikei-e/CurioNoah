773464c114ae2f78b93e0a6bc1712ef956c79db8
Kaikei-e <e_kaikei@protonmail.com>
2023-06-28 21:24:39 +0900

    CurioNoah: fixing zombie issue

--- terminate_all.sh
+++ terminate_all.sh
@@ -1,6 +1,18 @@
 #!/bin/sh
 
 # To terminate all services, run this script from the root directory of the project
+INSIGHIT_STREAM_CONTAINER_NAME=curionoah-insight_stream-1
+INFO_ISLAND_CONTAINER_NAME=curionoah-info_island-1
+
+
+# terminate Insight Stream
+docker exec -d $INSIGHIT_STREAM_CONTAINER_NAME /bin/bash -c "kill $(cat /usr/src/app/pidfile.txt)" &&
+
+# terminate Info Island
+docker exec -d $INFO_ISLAND_CONTAINER_NAME /bin/bash -c "kill $(cat /usr/src/app/vite_pid.txt)" &&
+
 
 # terminate FeedHarmony
-kill $(lsof -t -i:5100)
+kill $(lsof -t -i:5100) &&
+
+echo "All services are terminated"
