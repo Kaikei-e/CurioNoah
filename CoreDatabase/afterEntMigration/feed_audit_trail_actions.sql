0fbee1499e3eec125923b110f1d0c885037bb401
Kaikei-e <e_kaikei@protonmail.com>
2023-07-02 23:20:47 +0900

    CoreDatabase :clean up sql

--- CoreDatabase/afterEntMigration/feed_audit_trail_actions.sql
+++ CoreDatabase/afterEntMigration/feed_audit_trail_actions.sql
@@ -1,4 +1,4 @@
 
-curio_noah> INSERT INTO curio_noah.feed_audit_trail_actions (action) VALUES ('update')
-curio_noah> INSERT INTO curio_noah.feed_audit_trail_actions (action) VALUES ('delete')
-curio_noah> INSERT INTO curio_noah.feed_audit_trail_actions (action) VALUES ('failure')
+INSERT INTO curio_noah.feed_audit_trail_actions (action) VALUES ('update')
+INSERT INTO curio_noah.feed_audit_trail_actions (action) VALUES ('delete')
+INSERT INTO curio_noah.feed_audit_trail_actions (action) VALUES ('failure')
