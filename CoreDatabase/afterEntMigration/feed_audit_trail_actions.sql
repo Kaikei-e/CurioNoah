33bf039e28f0dc04a8506573621d5dd461b8aa1f
Kaikei-e <e_kaikei@protonmail.com>
2023-07-02 19:34:31 +0900

    CoreDatabase: made the initialization query sentence.

---
+++ CoreDatabase/afterEntMigration/feed_audit_trail_actions.sql
@@ -0,0 +1,3 @@
+INSERT INTO curio_noah.feed_audit_trail_actions (action) VALUES ('update')
+INSERT INTO curio_noah.feed_audit_trail_actions (action) VALUES ('delete')
+INSERT INTO curio_noah.feed_audit_trail_actions (action) VALUES ('failure')
