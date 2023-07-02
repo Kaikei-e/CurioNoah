684f0eeb11087e33d78d1c220dd1e4729fb77dfb
Kaikei-e <e_kaikei@protonmail.com>
2023-07-02 20:43:42 +0900

    CoreDatabase: fix sql

--- CoreDatabase/afterEntMigration/feed_audit_trail_actions.sql
+++ CoreDatabase/afterEntMigration/feed_audit_trail_actions.sql
@@ -1,3 +1,4 @@
-INSERT INTO curio_noah.feed_audit_trail_actions (action) VALUES ('update')
-INSERT INTO curio_noah.feed_audit_trail_actions (action) VALUES ('delete')
-INSERT INTO curio_noah.feed_audit_trail_actions (action) VALUES ('failure')
+
+curio_noah> INSERT INTO curio_noah.feed_audit_trail_actions (action) VALUES ('update')
+curio_noah> INSERT INTO curio_noah.feed_audit_trail_actions (action) VALUES ('delete')
+curio_noah> INSERT INTO curio_noah.feed_audit_trail_actions (action) VALUES ('failure')
