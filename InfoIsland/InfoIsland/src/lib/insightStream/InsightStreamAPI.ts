8f2b2653f202bf9749064692b3244f6dbfc40500
Kaikei-e <e_kaikei@protonmail.com>
2023-07-17 23:45:36 +0900

    InfoIsland: making search API

---
+++ InfoIsland/InfoIsland/src/lib/insightStream/InsightStreamAPI.ts
@@ -0,0 +1,57 @@
+enum Method {
+    GET = "GET",
+    POST = "POST",
+    PUT = "PUT",
+    DELETE = "DELETE"
+}
+
+const action = (inputMethod: Method) => {
+    switch (inputMethod) {
+        case Method.GET:
+            return "GET";
+        case Method.POST:
+            return "POST";
+        case Method.PUT:
+            return "PUT";
+        case Method.DELETE:
+            return "DELETE";
+        default:
+            throw new Error("Unsupported method: " + inputMethod);
+    }
+}
+
+export const insightStreamAPI = async (inputAction: string, url: string, body?: Object) => {
+    const apiURL = import.meta.env.VITE_INSIGHT_STREAM;
+    const origin = import.meta.env.VITE_ORIGIN;
+
+    const wantedAction = action(inputAction as Method);
+    if (wantedAction === Method.GET) {
+        const response = await fetch(apiURL + "/" + url, {
+            method: wantedAction,
+            headers: {
+                "Content-Type": "application/json",
+                Accept: "application/json",
+                Origin: origin,
+            }
+        });
+        return await response.json().catch((err) => {
+            console.log(err);
+            return false;
+        });
+    }else if (wantedAction === Method.POST) {
+        const response = await fetch(apiURL + "/" + url, {
+            method: wantedAction,
+            headers: {
+                "Content-Type": "application/json",
+                Accept: "application/json",
+                Origin: origin,
+            },
+            body: JSON.stringify(body)
+        });
+        return await response.json().catch((err) => {
+            console.log(err);
+            return false;
+        });
+    }
+
+}
