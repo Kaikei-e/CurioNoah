835f9ddc34dbe957fc0d334d237c7f498414342b
Kaikei-e <e_kaikei@protonmail.com>
2023-07-21 21:54:37 +0900

    InsightStream: making keyword search functionality

--- InsightStream/InsightStream/driver/entDriver/searchFeeds.go
+++ InsightStream/InsightStream/driver/entDriver/searchFeeds.go
@@ -4,10 +4,20 @@ import (
 	"context"
 	"insightstream/domain/searchWord"
 	"insightstream/ent"
+	entFeeds "insightstream/ent/feeds"
 )
 
 func SearchFeeds(searchWord searchWord.SearchWord, cl *ent.Client, ctx context.Context) ([]*ent.Feeds, error) {
-	feeds, err := cl.Feeds.Query().Select("title", "description").Limit(100).Where().All(ctx)
+	const limit = 100
+
+	feeds, err := cl.Feeds.Query().Select("title", "description").Limit(20).Where(
+		entFeeds.Or(
+			entFeeds.TitleContains(searchWord.Title),
+			entFeeds.DescriptionContains(searchWord.Description),
+		),
+	).Order(
+		ent.Desc("dt_updated"),
+	).Offset(limit).All(ctx)
 	if err != nil {
 		return nil, err
 	}
