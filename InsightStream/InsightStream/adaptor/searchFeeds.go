841a92ef7ce3d79648419f5e6787ec6f43ab8ad9
Kaikei-e <e_kaikei@protonmail.com>
2023-07-23 09:55:03 +0900

    InsightStream: format the description of feeds

--- InsightStream/InsightStream/adaptor/searchFeeds.go
+++ InsightStream/InsightStream/adaptor/searchFeeds.go
@@ -1,9 +1,11 @@
 package adaptor
 
 import (
+	"fmt"
 	"github.com/labstack/echo/v4"
 	"insightstream/domain/searchWord"
 	"insightstream/domain/searchedFeed"
+	"insightstream/driver/parser"
 	"insightstream/ent"
 	"insightstream/usecase/searchUsecase"
 )
@@ -24,5 +26,18 @@ func SearchFeeds(c echo.Context, cl *ent.Client) ([]searchedFeed.ByTitleOrDescri
 		return nil, err
 	}
 
-	return titleOrDescription, nil
+	var formattedFeeds []searchedFeed.ByTitleOrDescription
+	for _, tod := range titleOrDescription {
+		description, err := parser.HTMLToDoc(tod.Description)
+		if err != nil {
+			err := fmt.Errorf("failed to parse html: %v", err)
+			fmt.Println(err)
+			continue
+		}
+
+		tod.Description = description
+		formattedFeeds = append(formattedFeeds, tod)
+	}
+
+	return formattedFeeds, nil
 }
