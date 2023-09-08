f74dcf0385694199f6126dd9c4f116083f063fd6
Kaikei-e <e_kaikei@protonmail.com>
2023-09-09 02:14:39 +0900

    Ghostkeeper: still learning

--- oathkeeper/oath/config.go
+++ oathkeeper/oath/config.go
@@ -7,7 +7,7 @@ func GetOathConfig() oauth2.Config {
 		ClientID:     "dummy-id",
 		ClientSecret: "dummy-secret",
 		Scopes:       []string{"dummy1", "dummy2"},
-		RedirectURL:  "http://curionoah:4173",
+		RedirectURL:  "http://curionoah:4173/callback",
 		Endpoint: oauth2.Endpoint{
 			AuthURL:  "http://curionoah.com/auth",
 			TokenURL: "http://curionoah.com/token",
