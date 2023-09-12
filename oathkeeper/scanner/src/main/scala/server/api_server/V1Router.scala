87e6b8ea89c01fda0b86227141336c42adb14813
Kaikei-e <e_kaikei@protonmail.com>
2023-09-13 01:08:24 +0900

    Oathkeeper: comparing web frameworks which is the better

---
+++ oathkeeper/scanner/src/main/scala/server/api_server/V1Router.scala
@@ -0,0 +1,7 @@
+package server.api_server
+
+import org.scalatra.ScalatraServlet
+
+class V1 extends ScalatraServlet {
+
+}
---
+++ oathkeeper/scanner/src/main/scala/server/package.scala
@@ -0,0 +1,13 @@
+import cats.effect.IO
+import org.http4s.HttpRoutes
+import org.http4s.dsl.io._
+
+package server {
+	package system {
+		class System{}
+	}
+
+	package api_server {
+		class V1{}
+	}
+}
