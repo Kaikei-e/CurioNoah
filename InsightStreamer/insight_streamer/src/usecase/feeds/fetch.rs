9eacce56e74b49dc1e49fa8b83635e8c83f266f3
Kaikei-e <e_kaikei@protonmail.com>
2023-09-20 10:38:49 +0900

    InsightStreamer: learning about mocking and testing

--- InsightStreamer/insight_streamer/src/usecase/feeds/fetch.rs
+++ InsightStreamer/insight_streamer/src/usecase/feeds/fetch.rs
@@ -12,3 +12,34 @@ pub async fn follow_list_by_twenty(pool: DatabasePool) -> Result<Vec<FollowList>
         Err(e) => Err(anyhow!(e)),
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::domain::feeds::FeedElement;
+    use async_trait::async_trait;
+    use mockall::{automock, mock, predicate::*};
+
+    mock! {
+        pub RSSFeedRepository {}
+
+        #[async_trait]
+        impl RSSFeedRepositoryTrait for RSSFeedRepository {
+            async fn fetch_follow_list_by_twenty(&self) -> Result<Vec<FollowList>, sqlx::Error>;
+            async fn fetch_feeds(&self) -> Result<Vec<FeedElement>, sqlx::Error>;
+            async fn fetch_follow_list(&self) -> Result<Vec<FollowList>, sqlx::Error>;
+            async fn fetch_follow_list_with_offset(
+                &self,
+                offset: i32,
+            ) -> Result<Vec<FollowList>, sqlx::Error>;
+        }
+    }
+
+    #[tokio::test]
+    async fn test_success_follow_list_by_twenty() {
+        let mock_driver = MockRSSFeedRepository::new();
+        let result = mock_driver.fetch_follow_list_by_twenty();
+
+        todo!("implement test")
+    }
+}
