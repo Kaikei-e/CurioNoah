599d07a161ef86c40deb1c0cb2f048ec1c18ed71
Kaikei-e <e_kaikei@protonmail.com>
2023-10-01 01:54:02 +0900

    InsightStreamer: added tokio client

---
+++ InsightStreamer/insight_streamer/src/driver/rss_collector/driver.rs
@@ -0,0 +1,11 @@
+use std::error::Error;
+use rss::Channel;
+use std::net::TcpStream;
+use hyper::Request;
+use bytes::Bytes;
+use tokio::io::{self, AsyncWriteExt as _};
+
+async fn fetch_rss(url: hyper::Uri) -> Result<Channel, Box<dyn Error>> {
+
+
+}
