8bdfd080118a069a53bc8d4fa0602c878516f77e
Kaikei-e <e_kaikei@protonmail.com>
2023-12-09 01:22:11 +0900

    FeedOracle: Add V1Fetcher implementation

---
+++ FeedOracle/feed_oracle/rest/src/fetcher/v1fetcher.rs
@@ -0,0 +1,14 @@
+use axum::http::Error;
+
+
+trait Fetcher {
+    fn fetch_feed_link(&self, url: &str) -> Result<String, Error>;
+}
+
+struct V1Fetcher;
+
+impl Fetcher for V1Fetcher {
+    fn fetch_feed_link(&self, url: &str) -> Result<String, Error> {
+      todo!("fetch feed link")
+    }
+}
