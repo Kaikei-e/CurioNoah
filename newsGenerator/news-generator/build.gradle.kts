97e29e61eb2949169c851c44c15be9949bfb1e35
Kaikei-e <e_kaikei@protonmail.com>
2024-07-11 20:25:13 +0900

    news-generator: init project

---
+++ newsGenerator/news-generator/build.gradle.kts
@@ -0,0 +1,37 @@
+val kotlin_version: String by project
+val logback_version: String by project
+val prometheus_version: String by project
+
+plugins {
+    kotlin("jvm") version "2.0.0"
+    id("io.ktor.plugin") version "2.3.12"
+}
+
+group = "com.example"
+version = "0.0.1"
+
+application {
+    mainClass.set("com.example.ApplicationKt")
+
+    val isDevelopment: Boolean = project.ext.has("development")
+    applicationDefaultJvmArgs = listOf("-Dio.ktor.development=$isDevelopment")
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("io.ktor:ktor-server-core-jvm")
+    implementation("io.ktor:ktor-server-auth-jvm")
+    implementation("io.ktor:ktor-server-cors-jvm")
+    implementation("io.ktor:ktor-server-metrics-jvm")
+    implementation("io.ktor:ktor-server-metrics-micrometer-jvm")
+    implementation("io.micrometer:micrometer-registry-prometheus:$prometheus_version")
+    implementation("io.ktor:ktor-server-content-negotiation-jvm")
+    implementation("io.ktor:ktor-serialization-jackson-jvm")
+    implementation("io.ktor:ktor-server-netty-jvm")
+    implementation("ch.qos.logback:logback-classic:$logback_version")
+    testImplementation("io.ktor:ktor-server-tests-jvm")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
+}
---
+++ newsGenerator/news-generator/src/main/kotlin/com/example/plugins/Monitoring.kt
@@ -0,0 +1,32 @@
+package com.example.plugins
+
+import com.codahale.metrics.*
+import io.ktor.server.application.*
+import io.ktor.server.metrics.dropwizard.*
+import io.ktor.server.metrics.micrometer.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+import io.micrometer.prometheus.*
+import java.util.concurrent.TimeUnit
+
+fun Application.configureMonitoring() {
+    install(DropwizardMetrics) {
+        Slf4jReporter.forRegistry(registry)
+            .outputTo(this@configureMonitoring.log)
+            .convertRatesTo(TimeUnit.SECONDS)
+            .convertDurationsTo(TimeUnit.MILLISECONDS)
+            .build()
+            .start(10, TimeUnit.SECONDS)
+    }
+    val appMicrometerRegistry = PrometheusMeterRegistry(PrometheusConfig.DEFAULT)
+
+    install(MicrometerMetrics) {
+        registry = appMicrometerRegistry
+        // ...
+    }
+    routing {
+        get("/metrics-micrometer") {
+            call.respond(appMicrometerRegistry.scrape())
+        }
+    }
+}
