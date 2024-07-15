3b760b3326f1b773317e0b05442c0e9d662a3eba
Kaikei-e <e_kaikei@protonmail.com>
2024-07-15 21:19:32 +0900

    news-generator: rewrite test

--- newsGenerator/news-generator/src/test/kotlin/com/example/ApplicationTest.kt
+++ newsGenerator/news-generator/src/test/kotlin/com/example/ApplicationTest.kt
@@ -1,11 +1,11 @@
 package com.example
 
-import com.example.plugins.*
 import io.ktor.client.request.*
 import io.ktor.client.statement.*
 import io.ktor.http.*
 import io.ktor.server.testing.*
-import kotlin.test.*
+import kotlin.test.Test
+import kotlin.test.assertEquals
 
 class ApplicationTest {
     @Test
@@ -13,9 +13,9 @@ class ApplicationTest {
         application {
             configureRouting()
         }
-        client.get("/").apply {
+        client.get("/api/v1/systems/alive").apply {
             assertEquals(HttpStatusCode.OK, status)
-            assertEquals("Hello World!", bodyAsText())
+            assertEquals("keep alive", bodyAsText())
         }
     }
 }
