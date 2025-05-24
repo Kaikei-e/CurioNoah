889d322cbd44e69bcfd5ef3994ac05461ca968d0
Kaikei-e <e_kaikei@protonmail.com>
2025-05-24 16:12:02 +0900

    Appコンポーネントのコードをリファクタリングし、プロパティの記述を簡素化しました。また、Iconコンポーネントを追加し、ボタン内のアイコンを更新しました。

--- InfoIsland/InfoIsland/src/App.tsx
+++ InfoIsland/InfoIsland/src/App.tsx
@@ -1,70 +1,53 @@
 import "./App.css";
-import { Box, Flex, VStack, Spacer, Text, Link, Button } from "@chakra-ui/react";
+import { Box, Flex, VStack, Spacer, Text, Link, Button, Icon } from "@chakra-ui/react";
+import { FaArrowRight } from "react-icons/fa";
 
 export const App = () => (
   <Box
-    w={"100vw"}
-    h={"100vh"}
-    minW={"100%"}
-    minH={"100%"}
-    backgroundImage={"/images/ship-sunset.jpg"}
-    backgroundSize={"cover"}
-    backgroundRepeat={"no-repeat"}
+    w="100vw"
+    h="100vh"
+    minW="100%"
+    minH="100%"
+    backgroundImage="/images/ship-sunset.jpg"
+    backgroundSize="cover"
+    backgroundRepeat="no-repeat"
     p={10}
   >
-    <Flex minWidth="max-content" h={"100%"} w={"100%"}>
-      <VStack
-        align="flex-start"
-        spaceX={4}
-        spaceY={4}
-      >
+    <Flex minWidth="max-content" h="100%" w="100%">
+      <VStack align="flex-start" gap={4}>
         <Text
           fontFamily="Jost"
           fontWeight="regular"
           fontSize={{ base: "3xl", md: "4xl", lg: "80px" }}
           color="#FFF"
-          width={{ base: "100%", md: "100%", lg: "100%" }}
-          maxWidth="100%"
+          w="100%"
+          maxW="100%"
         >
-          <span>CurioNoah</span>
+          CurioNoah
           <br />
-          <Box
-            as="span"
-            fontWeight="light"
-            fontSize={{ base: "xl", md: "2xl", lg: "6xl" }}
-          >
+          <Box as="span" fontWeight="light" fontSize={{ base: "xl", md: "2xl", lg: "6xl" }}>
             Explore, Store, Extend
           </Box>
           <br />
-          <Box
-            as={"span"}
-            fontWeight="light"
-            fontSize={{ base: "xl", md: "2xl", lg: "6xl" }}
-          >
+          <Box as="span" fontWeight="light" fontSize={{ base: "xl", md: "2xl", lg: "6xl" }}>
             : by curiosity.
           </Box>
         </Text>
       </VStack>
       <Spacer />
-      <Flex flexDir={"column"} justifyContent={"flex-end"} p="4%">
+      <Flex flexDir="column" justifyContent="flex-end" p="4%">
         <Link href="/login">
           <Button
             size={{ base: "lg", sm: "sm", md: "md", lg: "lg" }}
             variant="outline"
-            color={"#FFFFFF"}
+            color="#FFFFFF"
             _hover={{ color: "#000000", bg: "#FFFFFF" }}
-            rightIcon={
-              <ArrowForwardIcon
-                boxSize={{ base: "xl", md: "2xl", lg: "20px" }}
-              />
-            }
           >
-            <Text
-              fontFamily="Jost"
-              fontSize={{ base: "md", md: "2md", lg: "2xl" }}
-            >
-              Explore
-            </Text>
+            <Icon 
+              as={FaArrowRight}
+              color="#000000"
+              boxSize={{ base: "1.5em", md: "1.5em", lg: "2em" }}
+            />
           </Button>
         </Link>
       </Flex>
